45a845bb694dde65fed85250c32b8f4347c79ecc
sneha-thomas1 <sneha.thomas@sjsu.edu>
2018-11-21 12:43:57 -0800

    Adding music player system to living room scene

---
+++ MysteryRoomDesignPatterns/Assets/Laptop/ZoomInLaptop.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseOver : MonoBehaviour
+{
+
+    public void OnMouseEnter()
+    {
+        Debug.Log("enter");
+        transform.localScale += new Vector3(3.1F, 3.1f, 3.1f); //adjust these values as you see fit
+    }
+
+
+    public void OnMouseExit()
+    {
+        Debug.Log("left");
+        transform.localScale = new Vector3(1, 1, 1);  // assuming you want it to return to its original size when your mouse leaves it.
+    }
+
+}
