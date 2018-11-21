8532321f75e6b85f91a25443a2cbec8342ed23fb
Student <akshay.pagar1994@gmail.com>
2018-11-21 15:28:06 -0800

    Uploading Composite and Command screens

---
+++ MysteryRoomDesignPatterns/Assets/MenuScript.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class MenuScript : MonoBehaviour {
+
+    public string[] menuStrings;
+    public string newGameLevel;
+
+    public Text[] menuText;
+    int selectIndex;
+    int prevIndex;
+
+    int axisDirectionPressed;
+    int axisDirection;
+
+    Color transparentColor;
+
+    void Start()
+    {
+        transparentColor = new Color(1f, 1f, 1f, 0.5f);
+        selectIndex = 0;
+        prevIndex = selectIndex;
+        InitializeText();
+        axisDirection = 0;
+        axisDirectionPressed = 0;
+    }
+
+    void Update()
+    {
+        CheckAxis();
+        CheckForArrowKeys();
+        CheckForConfirmButton();
+        ResetCheckAxis();
+    }
+
+    void CheckAxis()
+    {
+        if (Input.GetAxis("Vertical") == 1 && axisDirection != 1)
+        {
+            axisDirection = 1;
+            axisDirectionPressed = 1;
+        }
+        else if (Input.GetAxis("Vertical") == -1 && axisDirection != -1)
+        {
+            axisDirection = -1;
+            axisDirectionPressed = -1;
+        }
+        else if (Input.GetAxis("Vertical") == 0 && axisDirection != 0)
+        {
+            axisDirection = 0;
+            axisDirectionPressed = 0;
+        }
+    }
+
+    void ResetCheckAxis()
+    {
+        axisDirectionPressed = 0;
+    }
+
+    void UpdateMenu(int newIndex, int oldIndex)
+    {
+        menuText[oldIndex].color = transparentColor;
+        menuText[newIndex].color = Color.white;
+    }
+
+    void InitializeText()
+    {
+        for (int i = 0; i < menuStrings.Length; i++)
+        {
+            menuText[i].text = menuStrings[i];
+            if (i != selectIndex)
+                menuText[i].color = transparentColor;
+        }
+    }
+
+    void CheckForArrowKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || axisDirectionPressed == 1)
+        {
+            selectIndex--;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.RightArrow) || axisDirectionPressed == -1)
+        {
+            selectIndex++;
+        }
+        if (selectIndex > menuStrings.Length - 1)
+            selectIndex -= menuStrings.Length;
+        else if (selectIndex < 0)
+            selectIndex += menuStrings.Length;
+
+        if (selectIndex != prevIndex)
+        {
+            UpdateMenu(selectIndex, prevIndex);
+            prevIndex = selectIndex;
+        }
+    }
+
+    void CheckForConfirmButton()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return) || Input.GetButtonDown("PS4Jump"))
+        {
+            switch (selectIndex)
+            {
+                //New Game
+                case 0:
+                    SceneManager.LoadScene(newGameLevel);
+                    break;
+
+                //Continue
+                case 1:
+                    break;
+
+                //Options
+                case 2:
+                    Application.Quit();
+                    break;
+
+                //Quit
+                case 3:
+
+                    break;
+            }
+        }
+    }
+}
---
+++ MysteryRoomDesignPatterns/Assets/Scripts/Button.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+    public GameObject Light;
+
+	// Use this for initialization
+	void Start () {
+
+        Light.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Debug.Log("Mouse button clicked");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.name == "Button")
+                {
+                    Debug.Log("This is button");
+
+                    if (Light.activeInHierarchy == true)
+                        Light.SetActive(false);
+                    else
+                        Light.SetActive(true);
+                }
+            }
+        }
+	}
+}
