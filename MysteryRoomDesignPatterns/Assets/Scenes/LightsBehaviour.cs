9d1da24b65eb9aaa67b45f2abb756bf15368a3ed
nikitabairagi <nikitabairagi@gmail.com>
2018-11-21 09:19:05 -0800

    Added scene for observer pattern

---
+++ MysteryRoomDesignPatterns/Assets/Scenes/LightsBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightsBehaviour : MonoBehaviour {
+    private Light myLight;
+    private Light pl;
+    private GameObject[] pointLights;
+
+
+    // Use this for initialization
+    void Start() {
+        pointLights = GameObject.FindGameObjectsWithTag("pointlight");
+        myLight = GetComponent<Light>();
+        InvokeRepeating("ShiftToNight", 2.0f, 4.0f);
+        InvokeRepeating("ShiftToDay", 5.0f, 5.0f);
+
+    }
+    void ShiftToNight() {
+        myLight.enabled = false;
+        foreach (GameObject pointlight in pointLights)
+        {
+            pl = pointlight.GetComponent<Light>();
+            pl.enabled = true;
+
+        }
+    }
+
+    void ShiftToDay() {
+        myLight.enabled = true;
+        foreach (GameObject pointlight in pointLights)
+        {
+            pl = pointlight.GetComponent<Light>();
+            pl.enabled = false;
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+       
+
+    }
+}
