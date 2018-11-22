0a9db58f2c374b867704fc8a9acb4585436e7654
Student <akshay.pagar1994@gmail.com>
2018-11-21 20:32:48 -0800

    Updating scenes

---
+++ MysteryRoomDesignPatterns/Assets/Button2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button2 : MonoBehaviour {
+    public GameObject Lightb;
+    // Use this for initialization
+    void Start () {
+        Lightb.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Debug.Log("Mouse button clicked");
+            Ray ray1 = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit1;
+
+            if (Physics.Raycast(ray1, out hit1))
+            {
+                if (hit1.transform.name == "ButtonB")
+                {
+                    Debug.Log("This is button");
+
+                    if (Lightb.activeInHierarchy == true)
+                        Lightb.SetActive(false);
+                    else
+                        Lightb.SetActive(true);
+                }
+            }
+        }
+    }
+}
