63e8a4044a1dbf484c8e8c4ba35aafbb2958daae
Mayur Barge <bargemayur05@gmail.com>
2018-11-27 10:31:28 -0800

    Add hint script

---
+++ MysteryRoomDesignPatterns/Assets/Scripts/HintScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HintScript : MonoBehaviour {
+
+    public GameObject panel;
+    public Text Hint;
+    public bool show = false;
+  // Use this for initialization
+	void Start () {
+        panel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (show)
+        {
+            if(panel.activeInHierarchy == false)
+                panel.SetActive(true);
+            Hint.enabled = true;
+           
+        }
+        else
+        {
+            if (panel.activeInHierarchy == true)
+                panel.SetActive(false);
+            Hint.enabled = false;
+            
+        }
+    }
+
+    void ShowHint()
+    {
+        //Debug.Log("ShowHint clicked");
+        if (show)
+            show = false;
+        else
+            show = true;
+    }
+
+
+}
