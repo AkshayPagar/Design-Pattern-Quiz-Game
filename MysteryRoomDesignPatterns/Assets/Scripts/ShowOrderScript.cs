28d60c7abd738d6da1d61352cad7990577e6c7c6
Student <akshay.pagar1994@gmail.com>
2018-11-29 03:51:13 -0800

    adding kitchen animation

---
+++ MysteryRoomDesignPatterns/Assets/Scripts/ShowOrderScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowOrderScript : MonoBehaviour {
+    public GameObject Order;
+    // Use this for initialization
+    void Start () {
+        Order.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Debug.Log("Mouse button clicked");
+            Ray ray1 = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit1;
+
+            if (Physics.Raycast(ray1, out hit1))
+            {
+                if (hit1.transform.name == "Burger")
+                {
+                    Debug.Log("This is frame");
+
+                    if (Order.activeInHierarchy == true)
+                        Order.SetActive(false);
+                    else
+                        Order.SetActive(true);
+                }
+            }
+        }
+    }
+}
