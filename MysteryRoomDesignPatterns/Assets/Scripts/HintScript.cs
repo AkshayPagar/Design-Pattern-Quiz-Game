19e4e0e544132254ba605a592d8ca249da290ae3
Mayur Barge <bargemayur05@gmail.com>
2018-11-30 13:16:22 -0800

    State pattern changes

--- MysteryRoomDesignPatterns/Assets/Scripts/HintScript.cs
+++ MysteryRoomDesignPatterns/Assets/Scripts/HintScript.cs
@@ -7,22 +7,23 @@ public class HintScript : MonoBehaviour {
     public Text HintButton;
     public GameObject panel;
     public Text Hint;
-    public bool show = false;
-    public static int hints = 1;
-    // Use this for initialization
+    HintManager hintManager;
     void Start () {
+        Debug.Log("Init");
+        hintManager = HintManager.GetInstance();
         panel.SetActive(false);
+        Hint.enabled = false;
     }
 
     // Update is called once per frame
-    void Update() {
-        
-        if(hints>=0)
-          HintButton.text = "Hints: " + hints;
-        else
-            HintButton.text = "Hints: 0";
+    void Update() 
+    {
+        HintButton.text = "Hints: " + hintManager.GetHintCount();
+    }
 
-        if (show && hints>=0)
+    void ShowHint()
+    {
+        if (hintManager.GetHintCount().CompareTo(1) == 0)
         {
             if (panel.activeInHierarchy == false)
                 panel.SetActive(true);
@@ -30,27 +31,13 @@ public class HintScript : MonoBehaviour {
         }
         else
         {
-
             if (panel.activeInHierarchy == true)
+            {
                 panel.SetActive(false);
+            }
             Hint.enabled = false;
-            
         }
+        hintManager.Request();
     }
-
-    void ShowHint()
-    {
-
-        //Debug.Log("ShowHint clicked");
-        if (show)
-            show = false;
-        else
-        {
-            show = true;
-            if(hints>=0)
-            hints--;
-        }
-    }
-
-
 }
+
